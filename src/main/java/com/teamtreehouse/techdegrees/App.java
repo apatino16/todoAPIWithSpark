10920ea212a1af591575646ebda28b7c4c5af2d3
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-12-19 21:15:03 -0800

    Set up API routes

--- src/main/java/com/teamtreehouse/techdegrees/App.java
+++ src/main/java/com/teamtreehouse/techdegrees/App.java
@@ -1,17 +1,33 @@
 package com.teamtreehouse.techdegrees;
 
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import com.google.gson.Gson;
+import com.teamtreehouse.techdegrees.dao.Sql2oTodoDao;
+import com.teamtreehouse.techdegrees.dao.TodoDao;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
 
 public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/blah", (req, res) -> "Hello!");
-        // Comment
+        // JSON transformation
+        Gson gson = new Gson();
 
+        // Default response type is JSON
+        before("/api/v1/*", (req, res) -> res.type("application/json"));
+
+        // Initialize DAO
+        TodoDao todoDao = new Sql2oTodoDao(sql2o);
+
+        // Setup Routes
+        routes(todoDao, gson);
+    }
+
+    private static void routes(TodoDao todoDao, Gson gson){
+        
     }
 
 }
