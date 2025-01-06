637910b2121406d33441c41de669f73a0ed8a9a5
Anny Patino <103137175+apatino16@users.noreply.github.com>
2025-01-06 12:30:26 -0800

    Create ApiResponse.java

---
+++ src/test/java/com/teamtreehouse/techdegrees/testing/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.teamtreehouse.techdegrees.testing;
+
+public class ApiResponse {
+    private int status;
+    private String body;
+
+    public ApiResponse(int status, String body) {
+        this.status = status;
+        this.body = body;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
